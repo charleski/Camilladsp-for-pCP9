def0dbc61b97008875b52911e0338c58c3813b57
charleski <charleski2000@hotmail.com>
2024-04-26 14:45:57 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-cp tczs/* /etc/sysconfig/tcedir/optional
-tce-load -i camilladsp64-2.0.3.tcz camillagui-2.1.1.tcz
-if [ ! $(grep -Fq "opt/camillagui" /opt/.xfiletool.lst) ]
-then
-	echo opt/camillagui >> /opt/.xfiletool.lst
-fi
-
