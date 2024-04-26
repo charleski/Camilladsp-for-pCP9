1be6423f96438c0f8904172d5465850f382e6a9c
charleski <charleski2000@hotmail.com>
2024-04-26 18:01:07 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-cp tczs/* /etc/sysconfig/tcedir/optional
-echo -e "camilladsp64-2.0.3.tcz\ncamillagui-2.1.1.tcz" >> /etc/sysconfig/tcedir/onboot.lst
-if [ ! $(grep -Fq "opt/camillagui" /opt/.xfiletool.lst) ]
-then
-	echo opt/camillagui >> /opt/.xfiletool.lst
-fi
-
