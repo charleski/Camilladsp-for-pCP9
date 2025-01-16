868e45a91b0c51a982cb6d509c0326de15e64afb
charleski <charleski2000@hotmail.com>
2025-01-16 10:32:44 +0000

    Upgrade to CamillaDSP 3.0.0

---
+++ update_2.0.3-3.0.0.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+git clone -n --depth=1 --filter=tree:0 \
+  https://github.com/charleski/Camilladsp-for-pCP9.git
+cd Camilladsp-for-pCP9
+git sparse-checkout set --no-cone /tczs
+git checkout
+
+cp *.tcz /etc/sysconfig/tcedir/optional
+cp *.dep /etc/sysconfig/tcedir/optional
+
+sed -i 's/camilladsp64-2.\d.\d/camilladsp64-3.0.0' /etc/sysconfig/tcedir/onboot.lst
+sed -i 's/camillagui-2.\d.\d/camillagui-3.0.0' /etc/sysconfig/tcedir/onboot.lst
+
